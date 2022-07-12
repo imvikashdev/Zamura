5ccf2300d2159e6db2ebb43df0f13977fb049a57
vikash sirvi <vikashsirvi9999@gmail.com>
2022-07-12 19:06:30 +0530

    setting up video types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
 import type { NextPage } from "next";
+import axios from "axios";
+import { Video } from "../types";
+import VideoCard from "../components/VideoCard";
+import NoResults from "../components/NoResults";
 
-const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+interface Iprops {
+  videos: Video[];
+}
+
+const Home = ({ videos }: Iprops) => {
+  console.log(videos);
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={"No Videos"} />
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+  return {
+    props: {
+      videos: data,
+    },
+  };
 };
 
 export default Home;
