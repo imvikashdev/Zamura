9c5fa9c7437069206a3f172a2739e8adfe39fbe8
vikash sirvi <vikashsirvi9999@gmail.com>
2022-07-12 10:33:11 +0530

    discover and footer section added

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,9 @@ import GoogleLogin from "react-google-login";
 import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import { ImCancelCircle } from "react-icons/im";
 import { useState } from "react";
+import Discover from "./Discover";
+import SuggestedAccounts from "./SuggestedAccounts";
+import Footer from "./Footer";
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true);
