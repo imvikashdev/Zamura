31670525e41058a075f3f820abf5c3a5f1d0a0e2
vikash sirvi <vikashsirvi9999@gmail.com>
2022-07-13 13:20:44 +0530

    google auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import { useState, useEffect } from "react";
 const MyApp = ({ Component, pageProps }: AppProps) => {
@@ -15,7 +16,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if (isSSR) return null;
 
   return (
-    <div>
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
       <Navbar />
       <div className="flex gap-6 md:gap-20">
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
@@ -25,7 +26,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 };
 
