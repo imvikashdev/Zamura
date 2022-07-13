81721830552688326f73f3f3861b6a5979387719
vikash sirvi <vikashsirvi9999@gmail.com>
2022-07-13 11:33:02 +0530

    short player added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["yt3.ggpht.com", "randomuser.me"],
+  },
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
