6813a913a3ee2973afcc241ee7b53c818a02eb10
vikash sirvi <vikashsirvi9999@gmail.com>
2022-07-13 13:37:52 +0530

    google oauth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
+
 import { AiOutlineLogout } from "react-icons/ai";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
 import Logo from "../utils/tiktik-logo.png";
 const Navbar = () => {
+  const user = false;
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -20,6 +22,17 @@ const Navbar = () => {
           />
         </div>
       </Link>
+      <div>Search</div>
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => console.log(response)}
+            onError={() => console.log("error")}
+          />
+        )}
+      </div>
     </div>
   );
 };
