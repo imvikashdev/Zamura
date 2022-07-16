cf3d3bb597b1667bd584bde6412e91d217df4885
vikash sirvi <vikashsirvi9999@gmail.com>
2022-07-16 15:58:12 +0530

    creating user store

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["yt3.ggpht.com", "randomuser.me"],
+    domains: ["yt3.ggpht.com", "randomuser.me", "lh3.googleusercontent.com"],
   },
   swcMinify: true,
 };
